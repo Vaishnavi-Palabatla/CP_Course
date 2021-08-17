105ba403f8100e4cc38047e08d46fc6ee20ccad3
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-08-17 19:13:43 +0530

    coded

--- powersum.py
+++ powersum.py
@@ -10,28 +10,31 @@
 # If n is negative, return 0. Similarly, if k is negative, 
 # return 0.
 
-def power(a, b):
-    if b == 0:
+# def power(a, b):
+#     if b == 0:
+#         return 1
+#     else:
+#         return a * power(a, b-1)
+
+def power(x, y):
+     
+    if (y == 0):
         return 1
-    else:
-        return a * power(a, b-1)
-
-
-
-
-
-
-
-
-
-
-
+     
+    if (x == 0):
+        return 0
+     
+    return x * power(x, y - 1)
 
 def powerSum(n, k):
+    sum=0
     # Your code goes here...
-    return 0
-
+    for i in range(1,n+1):
+        sum=sum+power(i,k)
+    return sum
+print(powerSum(2,10))
+print(powerSum(3,10))
 # Write your own test cases here...
-assert(powerSum(2,10) == 1025)
-assert(powerSum(3,10) == 60074)
+# assert(powerSum(2,10) == 1025)
+# assert(powerSum(3,10) == 60074)
 print ("All test cases passed...")
