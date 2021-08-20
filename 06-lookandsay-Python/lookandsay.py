327bd5aa7af2a47c40f23b52059fb9c6f210d242
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-08-20 10:01:42 +0530

    coded

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -8,7 +8,49 @@
 # lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
-
 def lookandsay(a):
-	# Your code goes here
-	pass
+    # your code goes here
+	b=[]
+	c=[]
+	j=0
+	count=1
+	if(len(a)==0):
+		return b
+	for i in a:
+		if(i!=j):
+			b.append((count,j))
+			j=i
+			count=1
+		else:
+			count=count+1
+	print(i,j,count)
+	b.append((count,i))
+	return b[1:]
+
+# def lookandsay(a):
+# 	# Your code goes here
+# 	# l=[]
+# 	d={}
+# 	# for i in range(len(a)):
+# 	# 	if i not in l:
+# 	# 		l.append(i)
+# 	# for i in l:
+# 	# 	d[i]=a.count(i)
+	
+# 	for i in range(len(a)):
+# 		count=0
+# 		d[i]=count
+# 		for j in range(i+1,len(a)):
+# 			if(a[i]==a[j]):
+# 				# count=count+1
+# 				x=a[i]
+# 				print(d)
+# 				d[a[i]]=d[x]+1
+				
+# 			else:
+# 				# d[a[i]]=count
+# 				break
+
+# 	print(d)
+print(lookandsay([3,3,8,3,3,3,3]))
+	# pass
