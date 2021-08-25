1baec4e59c856a7291089f5fc31cabb89cb10e19
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-08-25 17:22:32 +0530

    coded

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -8,6 +8,12 @@
 # head down (so we say that "up is down")
 
 def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    return True
+    if((left1>=left2 and left1+width1<=left2) or (top1>=top2 and top1+height1<=top2)):
+        return False
+    elif((left1>=width2 and width2<=left1+width1) or (top1>=height2 and height2<=top1+height1)):
+        return False
+    else:
+        return True
+    # return True
 
         
