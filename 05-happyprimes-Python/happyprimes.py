580c23d6d37bef017160c47a8c1e04d42408a106
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-09-02 00:25:01 +0530

    coded

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,73 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
-def ishappyprimenumber(n):
-    # Your code goes here
-    pass
+# def sumOfSquaresOfDigits(n):
+#     s = str(n)
+#     Sum = 0
+#     for i in s:
+#         Sum += int(i)**2
+#     return Sum
+
+# def isPrime(n):
+#     if n <2:
+#         return False
+#     if n == 2:
+#         return True
+#     if n%2 == 0:
+#         return False
+#     maxfactor = round(n**0.5)
+#     for i in range(3,maxfactor+1,2):
+#         if n%i == 0:
+#             return False
+#     return True
+
+# def ishappyprimenumber(n):
+#     # Your code goes here
+#     if not isPrime(n):
+#         return False
+#     seen = set()
+#     seen.add(n)
+#     num = n
+#     while num!=1:
+#         Sum = sumOfSquaresOfDigits(num)
+#         if Sum in seen or Sum==4:
+#             return False
+#         seen.add(Sum)
+#         num = Sum
+#     return True
+
+def ishappyprimenumber(m):
+    if(ishappynumber(m) and isprime(m)):
+        return True
+    return False
+ 
+def ishappynumber(n):
+    if(n==1):
+        return True
+    while (n>=10):
+        n=squarenum(n)
+        if(n==1):
+            return True
+    return False
+ 
+def squarenum(m):
+    sum=0
+    while(m>0):
+        rem=m%10
+        sum+=(rem*rem)
+        m//=10
+    return sum
+ 
+def isprime(y):
+    if (y <= 1):
+        return False
+    if (y == 2):
+        return True
+    if (y%2==0):
+        return False
+    maxfactor=round(y**0.5)
+    for factor in range(3,maxfactor+1,2):
+        if (y%factor==0):
+            return False
+    return True
+
