d16b3f6d4861ad9f2fa1c7c52d202b6278d69f0b
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-09-02 00:10:09 +0530

    coded

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -9,4 +9,26 @@
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    s = ""
+    count = 0
+    l = []
+    for i in range(len(s1)):        
+        if s1[i] not in s2:
+            s = ""
+            continue
+        s += s1[i]
+        if s not in s2:
+            s = s[1:]
+        if s not in s2:
+            continue
+        if len(s)>=count:
+            count = len(s)
+            l.append(s)
+    cl = [i for i in l if len(i)==count]
+    if cl==[]:
+        return ""
+    lcs = "" + cl[0]
+    for i in cl:
+        if i<lcs:
+            lcs = i
+    return lcs
