8066fcb6ccc9618712daabecc7c94244cf1e530a
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-09-02 00:40:22 +0530

    coded

--- set.py
+++ set.py
@@ -7,6 +7,20 @@
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
+from itertools import combinations
+
 def limitedPowerSet(n, k):
     # Your code goes here...
-    pass
+    l = [i for i in range(1,n+1)]
+    powersets = []
+    for i in range(n):
+        comb = list(combinations(l,i))
+        comb = [set(i) for i in comb]
+        powersets.extend(comb)
+        if len(powersets)>k:
+            powersets = powersets[:k]
+    return powersets        
+
+assert limitedPowerSet(5,7)==[ set(), {1}, {2}, {3}, {4}, {5}, {1, 2} ]
+assert limitedPowerSet(5,8)==[ set(), {1}, {2}, {3}, {4}, {5}, {1, 2}, {1, 3} ]
+print("All testcases passed")
