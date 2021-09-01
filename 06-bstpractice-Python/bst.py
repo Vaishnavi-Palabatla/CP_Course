fde9ede4b501580cad550c9a751ca6e739d5a494
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-09-02 00:41:55 +0530

    coded

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,7 +10,20 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        newnode = Node(new_val)
+        current = self.root
+        while current:
+            prev = current
+            if (new_val < current.value):
+                current = current.left
+            else:
+                current = current.right
+        if (prev == None):
+            prev = newnode
+        elif new_val < prev.value:
+            prev.left = newnode
+        else:
+            prev.right = newnode
 
     def printSelf(self):
         # Your code goes here
@@ -18,5 +31,14 @@ class BST(object):
         
     def search(self, find_val):
         # Your code goes here
-        pass
-
+        root = self.root
+        while root:
+            if type(find_val)!=type(root.value):
+                return False
+            if find_val > root.value:
+                root = root.right    
+            elif find_val < root.value:
+                root = root.left
+            else:
+                return True
+        return False
